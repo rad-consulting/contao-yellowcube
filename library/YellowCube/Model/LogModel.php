e656efda61bccd99f233a567ea70e0f8d1226b69
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-23 14:17:09 +0100

    Initial

---
+++ library/YellowCube/Model/LogModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Contao extension for RAD Consulting GmbH
+ *
+ * @copyright  RAD Consulting GmbH 2016
+ * @author     Chris Raidler <c.raidler@rad-consulting.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+namespace YellowCube\Model;
+
+/**
+ * Class LogModel
+ */
+class LogModel
+{
+
+}
