30595ca956eb465e0635e11931004f3d1098e004
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-25 19:02:11 +0100

    Soap/Service

--- dca/tl_iso_producttype.php
+++ dca/tl_iso_producttype.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * Contao extension for RAD Consulting GmbH
- *
- * @copyright  RAD Consulting GmbH 2016
+ * @copyright  RAD Consulting GmbH 2017
  * @author     Chris Raidler <c.raidler@rad-consulting.ch>
+ * @author     Olivier Dahinden <o.dahinden@rad-consulting.ch>
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
-// YellowCube
+// Palettes
 $GLOBALS['TL_DCA']['tl_iso_producttype']['palettes']['yellowcube'] = $GLOBALS['TL_DCA']['tl_iso_producttype']['palettes']['standard'];
