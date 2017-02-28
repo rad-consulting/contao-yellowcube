724ebce749d25200ab1bf34bdf3765ba0aacd351
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-28 12:56:03 +0100

    Soap/Service

--- library/RAD/YellowCube/Service.php
+++ library/RAD/YellowCube/Service.php
@@ -281,7 +281,7 @@ class Service implements EventSubscriber
                 return;
             }
 
-            throw new LogException('No delivery', Log::ERROR, $this->getLastXML());
+            throw new LogException('No delivery', Log::ERROR, null, $this->getLastXML());
         }
     }
 
