83e4d874f1526f9c29c26cbaa38f470b1d99009b
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-03-03 12:24:13 +0100

    YellowCube

--- languages/de/tl_settings.php
+++ languages/de/tl_settings.php
@@ -11,10 +11,10 @@
 $GLOBALS['TL_LANG']['tl_settings']['rad_yellowcube_legend'] = 'YellowCube';
 
 // Fields
-$GLOBALS['TL_LANG']['tl_settings']['rad_yellowcube_active'] = array('Aktivieren');
+$GLOBALS['TL_LANG']['tl_settings']['rad_yellowcube_active'] = array('Aktiv', 'Aktivieren oder deaktivieren Sie die Schnittstelle.');
 $GLOBALS['TL_LANG']['tl_settings']['rad_yellowcube_sender'] = array('Sender');
 $GLOBALS['TL_LANG']['tl_settings']['rad_yellowcube_depositorno'] = array('Depositor Number');
 $GLOBALS['TL_LANG']['tl_settings']['rad_yellowcube_supplierno'] = array('Supplier Number');
-$GLOBALS['TL_LANG']['tl_settings']['rad_yellowcube_operatingmode'] = array('Operating Mode');
+$GLOBALS['TL_LANG']['tl_settings']['rad_yellowcube_operatingmode'] = array('Operating Mode', 'Modus der Schnittstelle.');
 $GLOBALS['TL_LANG']['tl_settings']['rad_yellowcube_partnerid'] = array('Partner ID');
 $GLOBALS['TL_LANG']['tl_settings']['rad_yellowcube_plantid'] = array('Plant ID');
