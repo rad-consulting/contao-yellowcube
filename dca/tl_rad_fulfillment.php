a1c81e136cb1e8dfd56a054456de5fabf7f429a6
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-03-05 10:15:25 +0100

    YellowCube

--- dca/tl_rad_fulfillment.php
+++ dca/tl_rad_fulfillment.php
@@ -9,3 +9,6 @@
 
 // Config
 $GLOBALS['TL_DCA']['tl_rad_fulfillment']['config']['onsubmit_callback'][] = array('RAD\\YellowCube\\Backend\\Panel', 'onSubmit');
+
+// Palettes
+$GLOBALS['TL_DCA']['tl_rad_fulfillment']['palettes']['yellowcube'] = '{type_legend},id,pid,type,status;{fulfillment_legend},reference,delivery,tracking';
